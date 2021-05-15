524452b20576685734b0ea9596545202eaa88264
Artabovereason <pierreguichard.pro@gmail.com>
2021-05-16 00:25:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,8 +11,7 @@
 
 using namespace std;
 
-//int const town_number=7;
-#define town_number 11
+#define town_number 10
 
 double fact(int n){
 
@@ -34,40 +33,37 @@ vector<double> vertex;
 for (int i = 0; i < town_number; i++)
         if (i != s)
             vertex.push_back(i);
-  /*array<int, town_number-1> vertex{
-    (s + 1) % town_number,
-    (s + 2) % town_number,
-    (s + 3) % town_number*/
-
-/* for (int i = 0; i < town_number; i++)
-    if (i != s){
-        vertex[i]; */
 
   double min_path = INT_MAX;
-  do {
+  do
+  {
 
     double current_pathweight = 0;
 
     int k = s;
 
-    for (int i = 0; i < vertex.size(); i++) {
-
-      //cout<<k<<" "<<current_pathweight<<endl; ////////////
+    for (int i = 0; i < vertex.size(); i++)
+    {
       longueur_chemin.push_back(k);
       int A= vertex[i];
       current_pathweight += matrix[k][A];
       k = vertex[i];
-      if (int j=i == vertex.size()-1){
-
+      if (int j=i == vertex.size()-1)
+      {
         longueur_chemin.push_back(k);
-        //cout<<k<<" "<<current_pathweight<<endl; ////////////
       }
+    }
+    t=(clock() - t);
+    float temps =((float)t)/CLOCKS_PER_SEC;
+/*
+ if ( temps>0.05 && temps <3){
+    //  cout<<((float)t)/CLOCKS_PER_SEC<<endl;
+      cout<<"Le programme de ses morts run en "<< (2*temps/((longueur_chemin.size()/fact(town_number))))<<"secondes"<<endl;
+     break;
+   } */
 
 
-    }
-    if (t = clock() - t==4){
-      cout<<"oui"<<endl;
-    }
+
     current_pathweight += matrix[k][s];
     //cout<<"total : "<<current_pathweight<<endl; ////////////
     //cout<<" "<<endl;
@@ -76,38 +72,33 @@ for (int i = 0; i < town_number; i++)
 
 //cout<<(longueur_chemin.size()/fact(town_number+1))*1000<<endl;
 
-    // update minimum
-
     min_path = min(min_path, current_pathweight);
 
-  } while (
+  }
+  while(
     next_permutation(vertex.begin(), vertex.end()));
 
 
-for (int i =town_number; i< longueur_chemin.size();i=i+town_number+1){
+for (int i =town_number; i< longueur_chemin.size();i=i+town_number+1)
+{
   //cout<<longueur_chemin[i]<<endl;
-    if (longueur_chemin[i]==min_path){
+    if (longueur_chemin[i]==min_path)
+  {
       //cout<<"Le chemin suivi est alors : "<<longueur_chemin[i-4]<<" "<<longueur_chemin[i-3]<<" "<<longueur_chemin[i-2]<<" "<<longueur_chemin[i-1]<<endl;
       ofstream optimal_path("optimal_path.txt");
-    for (int j =i-town_number; j<i;j++ ){
+    for (int j =i-town_number; j<i;j++ )
+    {
          if (j+1==i){
         optimal_path<<towns[longueur_chemin[j]][0]<<" "<<towns[longueur_chemin[j]][1]<<" "<<towns[longueur_chemin[i-town_number]][0]<<" "<<towns[longueur_chemin[i-town_number]][1]<<endl;
         }
         else{
           optimal_path<<towns[longueur_chemin[j]][0]<<" "<<towns[longueur_chemin[j]][1]<<" "<<towns[longueur_chemin[j+1]][0]<<" "<<towns[longueur_chemin[j+1]][1]<<endl;}
 
-  /*    int a = longueur_chemin[i-4];
-      int b = longueur_chemin[i-3];
-      int c = longueur_chemin[i-2];
-      int d = longueur_chemin[i-1];
-      optimal_path<<towns[a][0]<<" "<<towns[a][1]<<" "<<towns[b][0]<<" "<<towns[b][1]<<endl;
-      optimal_path<<towns[b][0]<<" "<<towns[b][1]<<" "<<towns[c][0]<<" "<<towns[c][1]<<endl;
-      optimal_path<<towns[c][0]<<" "<<towns[c][1]<<" "<<towns[d][0]<<" "<<towns[d][1]<<endl;
-      optimal_path<<towns[d][0]<<" "<<towns[d][1]<<" "<<towns[a][0]<<" "<<towns[a][1]<<endl; */
-    }}
+    }
+  }
 
 }
-
+cout<<longueur_chemin.size()<<endl;
   return min_path;
 }
 
@@ -117,18 +108,30 @@ double findMax(double mat[town_number][town_number])
 
     double maxElement = INT_MIN;
 
-    for (int i = 0; i < town_number; i++) {
-        for (int j = 0; j < town_number; j++) {
-            if (mat[i][j] > maxElement) {
+    for (int i = 0; i < town_number; i++)
+    {
+        for (int j = 0; j < town_number; j++)
+        {
+            if (mat[i][j] > maxElement)
+            {
                 maxElement = mat[i][j];
             }
         }
     }
 
-    // finally return maxElement
     return maxElement;
 }
 
+double findMean(double a[town_number][town_number])
+{
+    double sum = 0;
+
+    for (int i=0; i<town_number; i++)
+       for (int j=0; j<town_number; j++)
+          sum += a[i][j];
+
+    return (double)sum/(town_number*town_number);
+}
 
 
 
@@ -142,14 +145,14 @@ int main()
     {{ 2, 2 }},
     {{ 3, 5 }},
     {{ 7, 2 }},
-    {{ 3, 4 }},
+    {{ -10, 21 }},
     {{ -2, 4 }},
-    {{ -4, 4 }},
+    {{ -12, 4 }},
     {{ 5, 4 }},
     {{ 12, 3 }},
     {{ 4, 8 }},
-    {{ 17, -2 }},
-    {{ -3, -4 }}/*,
+    {{ 17, -2 }}/*,
+    {{ -3, -4 }},
     {{ 7, -4 }},
     {{ -8, 4 }},
     {{ 15, -9 }},
@@ -158,34 +161,36 @@ int main()
     {{ 7, 12 }},
     {{ 13, -3 }},
     {{ -9, -4 }},
-    {{ -1, 4 }}*/
+    {{ -1, 4 }}*/ //20
   };
+
+
 
 double matrix[town_number][town_number]; //= {0,10,15,20,10,0,35,25,15,35,0,30,20,25,30,0};
 
 
 for ( int i = 0; i < town_number; i++ )
-      for ( int j = 0; j < town_number; j++ ) {
-
+      for ( int j = 0; j < town_number; j++ )
+      {
 matrix[i][j]=distance_town(towns[i], towns[j]) ;
-
       }
 
 
-
+array<string,10> name_town = {"Paris","Moscou","Strasbourg","Dijon","Toulouse","Montpellier","Rennes","Marseille","Berlin","Brest"};
 
 ofstream coordonnee("coordonnee.txt");
 for (int i =0;i<town_number;i++)
 {
-      coordonnee<<towns[i][0]<<" "<<towns[i][1]<<endl;
+      coordonnee<<towns[i][0]<<" "<<towns[i][1]<<" "<<name_town[i]<<endl;
 }
 
 ofstream coordonnee_bloc("coordonnee_bloc.txt");
-for ( int j = 0; j < town_number; j++ ){
-    for (int i =0;i<town_number;i++)
+for ( int j = 0; j < town_number; j++ )
 {
-      coordonnee_bloc<<towns[j][0]<<" "<<towns[j][1]<<" "<<towns[(i+1)%town_number][0]<<" "<<towns[(i+1)%town_number][1] <<endl;
-}
+    for (int i =0;i<town_number;i++)
+    {
+      coordonnee_bloc<<towns[j][0]<<" "<<towns[j][1]<<" "<<towns[(i+1)%town_number][0]<<" "<<towns[(i+1)%town_number][1]<<endl;
+    }
 coordonnee_bloc<<" "<<endl;
 coordonnee_bloc<<" "<<endl;
 }
@@ -198,13 +203,33 @@ cout << travllingSalesmanProblem(matrix, s,towns,t) << endl;
   t = clock() - t;
 printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 
-//cout<<findMax(matrix)<<endl;
+//cout<<findMean(matrix)<<endl;
+
+double sum0=0;
+for(int i = 0; i < town_number; i++){
+   sum0 += towns[i][0];
+}
+double xmean =  sum0 / town_number;
+
+double sum1=0;
+for(int i = 0; i < town_number; i++){
+   sum1 += towns[i][1];
+}
+double ymean =  sum1 / town_number;
+
+
+
+
+cout<<xmean<<" "<<ymean<<" "<< findMax(matrix)<<endl;
+
 
 ofstream town_graphic("town_graphic.gnu");
-   town_graphic<<"set xrange ["<<-findMax(matrix)<<":"<<findMax(matrix)<<"]"<<endl;
+   town_graphic<<"set offset 1,1,1,1"<<endl;
+   town_graphic<<"set style fill transparent solid 0.5"<<endl;
+   town_graphic<<"set xrange ["<<xmean-findMax(matrix)/1.3<<":"<<xmean+findMax(matrix)/1.3<<"]"<<endl;
    //town_graphic<<"set xtics font \"Verdana,15\""<<endl;
-   town_graphic<<"set yrange ["<<-findMax(matrix)<<":"<<findMax(matrix)<<"]"<<endl;
-   town_graphic<<"plot 'coordonnee.txt' pt 7 ps 4, \"coordonnee_bloc.txt\" using 1:2:($3-$1):($4-$2) with vectors nohead, 'optimal_path.txt' using 1:2:($3-$1):($4-$2) with vectors nohead linecolor rgb \"red\" "<<endl;
+   town_graphic<<"set yrange ["<<ymean-findMax(matrix)/1.3<<":"<<ymean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic<<"plot 'coordonnee.txt' using 1:2:3 with labels point  pt 7 offset char 0.5,0.5 notitle , \"coordonnee_bloc.txt\" using 1:2:($3-$1):($4-$2) with vectors nohead, 'optimal_path.txt' using 1:2:($3-$1):($4-$2) with vectors nohead linecolor rgb \"red\" "<<endl; // \"coordonnee_bloc.txt\" using 1:2:($3-$1):($4-$2) with vectors nohead,
    //town_graphic<<"plot 'coordonnee.txt' with points pt 7"<<endl;
    town_graphic<< "pause -1" <<endl;
    town_graphic<<"set terminal postscript eps size 3.5,2.62 enhanced color"<<endl;
@@ -212,7 +237,6 @@ ofstream town_graphic("town_graphic.gnu");
    town_graphic<<"replot"<<endl;
    system("gnuplot town_graphic.gnu");
 
-//\"coordonnee_bloc.txt\" using 1:2:($3-$1):($4-$2) with vectors nohead,
 
 
 
