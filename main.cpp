4a95b3bdf430b105c01b5fdf94445814df2929fb
Artabovereason <pierreguichard.pro@gmail.com>
2021-05-16 18:05:58 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -53,22 +53,19 @@ for (int i = 0; i < town_number; i++)
     }
     t=(clock() - t);
     float temps =((float)t)/CLOCKS_PER_SEC;
+
 /*
  if ( temps>0.05 && temps <3){
     //  cout<<((float)t)/CLOCKS_PER_SEC<<endl;
       cout<<"Le programme de ses morts run en "<< (2*temps/((longueur_chemin.size()/fact(town_number))))<<"secondes"<<endl;
      break;
-   } */
-
+   }
+*/
 
 
     current_pathweight += matrix[k][s];
-    //cout<<"total : "<<current_pathweight<<endl; ////////////
-    //cout<<" "<<endl;
     longueur_chemin.push_back(current_pathweight);
 
-
-//cout<<(longueur_chemin.size()/fact(town_number+1))*1000<<endl;
 
     min_path = min(min_path, current_pathweight);
 
@@ -100,6 +97,81 @@ cout<<longueur_chemin.size()<<endl;
   return min_path;
 }
 
+double travllingSalesmanProblemMax(double matrix[town_number][town_number], int s, array<array<double, 2>,town_number> towns,clock_t t)
+{
+
+
+vector<double> longueur_chemin_max;
+
+vector<double> vertex;
+for (int i = 0; i < town_number; i++)
+        if (i != s)
+            vertex.push_back(i);
+
+  double max_path = INT_MIN;
+  do
+  {
+
+    double current_pathweight = 0;
+
+    int k = s;
+
+    for (int i = 0; i < vertex.size(); i++)
+    {
+      longueur_chemin_max.push_back(k);
+      int A= vertex[i];
+      current_pathweight += matrix[k][A];
+      k = vertex[i];
+      if (int j=i == vertex.size()-1)
+      {
+        longueur_chemin_max.push_back(k);
+      }
+    }
+    t=(clock() - t);
+    float temps =((float)t)/CLOCKS_PER_SEC;
+
+/*
+ if ( temps>0.05 && temps <3){
+    //  cout<<((float)t)/CLOCKS_PER_SEC<<endl;
+      cout<<"Le programme de ses morts run en "<< (2*temps/((longueur_chemin.size()/fact(town_number))))<<"secondes"<<endl;
+     break;
+   }
+*/
+
+
+    current_pathweight += matrix[k][s];
+    longueur_chemin_max.push_back(current_pathweight);
+
+
+    max_path = max(max_path, current_pathweight);
+
+  }
+  while(
+    next_permutation(vertex.begin(), vertex.end()));
+
+
+for (int i =town_number; i< longueur_chemin_max.size();i=i+town_number+1)
+{
+  //cout<<longueur_chemin[i]<<endl;
+    if (longueur_chemin_max[i]==max_path)
+  {
+      //cout<<"Le chemin suivi est alors : "<<longueur_chemin[i-4]<<" "<<longueur_chemin[i-3]<<" "<<longueur_chemin[i-2]<<" "<<longueur_chemin[i-1]<<endl;
+      ofstream optimal_path_max("optimal_pathmax.txt");
+    for (int j =i-town_number; j<i;j++ )
+    {
+         if (j+1==i){
+        optimal_path_max<<towns[longueur_chemin_max[j]][0]<<" "<<towns[longueur_chemin_max[j]][1]<<" "<<towns[longueur_chemin_max[i-town_number]][0]<<" "<<towns[longueur_chemin_max[i-town_number]][1]<<endl;
+        }
+        else{
+          optimal_path_max<<towns[longueur_chemin_max[j]][0]<<" "<<towns[longueur_chemin_max[j]][1]<<" "<<towns[longueur_chemin_max[j+1]][0]<<" "<<towns[longueur_chemin_max[j+1]][1]<<endl;}
+
+    }
+  }
+
+}
+cout<<longueur_chemin_max.size()<<endl;
+  return max_path;
+}
 
 double findMax(double mat[town_number][town_number])
 {
@@ -164,7 +236,7 @@ int main()
 
 
 
-double matrix[town_number][town_number]; //= {0,10,15,20,10,0,35,25,15,35,0,30,20,25,30,0};
+double matrix[town_number][town_number];
 
 
 for ( int i = 0; i < town_number; i++ )
@@ -199,10 +271,11 @@ t = clock();
 
 cout << travllingSalesmanProblem(matrix, s,towns,t) << endl;
   t = clock() - t;
-printf ("It took me %d clicks (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
+printf ("It took me %d clicks for min (%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 
-//cout<<findMean(matrix)<<endl;
-
+cout << travllingSalesmanProblemMax(matrix, s,towns,t) << endl;
+  t = clock() - t;
+printf ("It took me %d clicks for max(%f seconds).\n",t,((float)t)/CLOCKS_PER_SEC);
 double sum0=0;
 for(int i = 0; i < town_number; i++){
    sum0 += towns[i][0];
@@ -215,12 +288,9 @@ for(int i = 0; i < town_number; i++){
 }
 double ymean =  sum1 / town_number;
 
-
-
-
 cout<<xmean<<" "<<ymean<<" "<< findMax(matrix)<<endl;
 
-
+/*
 ofstream town_graphic("town_graphic.gnu");
    town_graphic<<"set offset 1,1,1,1"<<endl;
    town_graphic<<"set style fill transparent solid 0.5"<<endl;
@@ -234,11 +304,61 @@ ofstream town_graphic("town_graphic.gnu");
    town_graphic<<"set output \"carte.eps\""<<endl;
    town_graphic<<"replot"<<endl;
    system("gnuplot town_graphic.gnu");
+*/
 
 
+ofstream town_graphic_multi("town_graphic_multi.gnu");
+town_graphic_multi<<"set terminal postscript eps size 7,5.24 enhanced color"<<endl;
+town_graphic_multi<<"set output \"cartefull.eps\""<<endl;
+   town_graphic_multi<<"set multiplot layout 2,2 rowsfirst"<<endl;
+   town_graphic_multi<<"set key font \"Verdana,12\""<<endl;
+   town_graphic_multi<<"set xtics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set ytics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set title \"Representation de l'Univers \" font \"Verdana,13\""<<endl;
+   town_graphic_multi<<"set xlabel \"x\""<<endl;
+   town_graphic_multi<<"set xrange ["<<xmean-findMax(matrix)/1.3<<":"<<xmean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set ylabel \"y\""<<endl;
+   town_graphic_multi<<"set yrange ["<<ymean-findMax(matrix)/1.3<<":"<<ymean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set style fill transparent solid 0.2 noborder"<<endl;
+   town_graphic_multi<<"plot 'coordonnee.txt' using 1:2:3 with labels point  pt 7 offset char 0.5,0.5 notitle"<<endl;
 
+   town_graphic_multi<<"set key font \"Verdana,12\""<<endl;
+   town_graphic_multi<<"set xtics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set ytics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set title \"Representation de tous les chemins\""<<endl;
+   town_graphic_multi<<"set xlabel \"x\""<<endl;
+   town_graphic_multi<<"set xrange ["<<xmean-findMax(matrix)/1.3<<":"<<xmean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set ylabel \"y point\""<<endl;
+   town_graphic_multi<<"set yrange ["<<ymean-findMax(matrix)/1.3<<":"<<ymean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set style fill transparent solid 0.2 noborder"<<endl;
+   town_graphic_multi<<"plot \"coordonnee_bloc.txt\" using 1:2:($3-$1):($4-$2) with vectors nohead  linecolor rgb \"grey\"  title \"Chemins\",'coordonnee.txt' with points pt 7 title \"ville\" "<<endl;
 
+   town_graphic_multi<<"set key font \"Verdana,12\""<<endl;
+   town_graphic_multi<<"set xtics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set ytics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set title \"Representation du chemin optimal\""<<endl;
+   town_graphic_multi<<"set xlabel \"x\""<<endl;
+   town_graphic_multi<<"set xrange ["<<xmean-findMax(matrix)/1.3<<":"<<xmean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set ylabel \"y\""<<endl;
+   town_graphic_multi<<"set yrange ["<<ymean-findMax(matrix)/1.3<<":"<<ymean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set style fill transparent solid 0.2 noborder"<<endl;
+   town_graphic_multi<<"plot 'optimal_path.txt' using 1:2:($3-$1):($4-$2) with vectors nohead linecolor rgb \"blue\" title \"Chemin optimal\",'coordonnee.txt' with points pt 7 title \"ville\""<<endl;
 
+   town_graphic_multi<<"set key font \"Verdana,12\""<<endl;
+   town_graphic_multi<<"set xtics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set ytics font \"Verdana,15\""<<endl;
+   town_graphic_multi<<"set title \"Representation du chemin maximal\""<<endl;
+   town_graphic_multi<<"set xlabel \"x\""<<endl;
+   town_graphic_multi<<"set xrange ["<<xmean-findMax(matrix)/1.3<<":"<<xmean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set ylabel \"y\""<<endl;
+   town_graphic_multi<<"set yrange ["<<ymean-findMax(matrix)/1.3<<":"<<ymean+findMax(matrix)/1.3<<"]"<<endl;
+   town_graphic_multi<<"set style fill transparent solid 0.2 noborder"<<endl;
+   town_graphic_multi<<"plot 'optimal_pathmax.txt' using 1:2:($3-$1):($4-$2) with vectors nohead linecolor rgb \"red\" title \"Chemin maximal\",'coordonnee.txt' with points pt 7 title \"ville\""<<endl;
+
+   town_graphic_multi<<"pause -1"<<endl; // le graphique reste affiché tant que je ne le ferme pas
+
+   town_graphic_multi<<"replot"<<endl;
+   system("gnuplot town_graphic_multi.gnu");
 
 
 
